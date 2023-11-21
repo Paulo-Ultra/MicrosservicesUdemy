cc86aec26e66ac3b5205ab53f171c17ed76b2029
Paulo Ricardo Freire Ultra <prfultra@yahoo.com.br>
2023-11-21 18:27:23 -0300

    Load Balancing

--- user-api/user-api/src/main/java/br/com/paulo/userapi/services/impl/UserServiceImpl.java
+++ user-api/user-api/src/main/java/br/com/paulo/userapi/services/impl/UserServiceImpl.java
@@ -5,18 +5,23 @@ import br.com.paulo.userapi.repositories.UserRepository;
 import br.com.paulo.userapi.services.UserService;
 import br.com.paulo.userapi.services.exceptions.ObjectNotFoundException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
+    private final Environment environment;
 
     @Override
     public User findById(Long id) {
+        log.info("USER_SERVICE ::: Get request on " + environment.getProperty("local.server.port") + " port");
         return userRepository.findById(id).orElseThrow(() -> new ObjectNotFoundException("Object not found"));
     }
 
